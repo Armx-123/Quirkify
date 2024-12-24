97969f191e9d91c2a0b1347e484f9cac0b1597ac
Arnav Chauhan <114233771+Armx-123@users.noreply.github.com>
2024-12-24 11:34:52 +0530

    Update unzip.py

--- unzip.py
+++ unzip.py
@@ -18,3 +18,5 @@ def unzip_file(zip_file_path, extract_to='.'):
 zip_file_path = 'zip.zip'  # replace with your file path
 extract_to = ''  # replace with your desired extraction path
 unzip_file(zip_file_path, extract_to)
+
+subprocess.run(["python", "main.py"], check=True)
